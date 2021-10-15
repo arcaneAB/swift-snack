fc426a3b1a5e6c288afd30c0ed6ee2b9b7be5e44
Anirudh4583 <anirudhmitra210@gmail.com>
2021-10-15 12:12:07 +0530

    project initialized

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Login from './components/auth/Login'
+import Home from './components/home/Home'
+import ForgotPass from './components/auth/ForgotPass'
+import { AuthProvider } from './contexts/AuthContext'
+// import PrivateRoute from './utils/PrivateRoute'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <Route exact path="/" component={Home} />
+
+            <Route exact path="/auth/login" component={Login} />
+            <Route exact path="/auth/forgot-password" component={ForgotPass} />
+
+            {/* <PrivateRoute path="/payment" component={payment} /> */}
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </>
+  )
+}
+
+export default App
