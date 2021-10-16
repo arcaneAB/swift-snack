345f4667465d6b3a161e49782a1767ae70f18963
Abhay Singh Chauhan <75360601+arcaneAB@users.noreply.github.com>
2021-10-16 12:35:18 +0530

    push

---
+++ cheff/src/components/orders.js
@@ -0,0 +1,79 @@
+const orders = [
+  {
+    id: 1,
+    table: 7,
+    orderList: [
+      {
+        orderID: 1,
+        dish: 'Dhokla',
+      },
+      {
+        orderID: 2,
+        dish: 'Kachori',
+      },
+      {
+        orderID: 3,
+        dish: 'Pulao',
+      },
+      {
+        orderID: 4,
+        dish: 'Dosa',
+      },
+    ],
+  },
+  {
+    id: 2,
+    table: 3,
+    orderList: [
+      {
+        orderID: 1,
+        dish: 'Mishti Doi',
+      },
+      {
+        orderID: 2,
+        dish: 'Gulab Jamun',
+      },
+      {
+        orderID: 3,
+        dish: 'Chole Bhature',
+      },
+    ],
+  },
+  {
+    id: 3,
+    table: 1,
+    orderList: [
+      {
+        orderID: 1,
+        dish: 'Water',
+      },
+      {
+        orderID: 2,
+        dish: 'Air',
+      },
+      {
+        orderID: 3,
+        dish: 'Time',
+      },
+      {
+        orderID: 4,
+        dish: 'Space',
+      },
+    ],
+  },
+  {
+    id: 4,
+    table: 5,
+    orderList: [
+      {
+        orderID: 1,
+        dish: 'Dhokla',
+      },
+      {
+        orderID: 2,
+        dish: 'Kachori',
+      }
+    ],
+  },
+]
+export default orders;
