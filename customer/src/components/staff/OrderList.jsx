b76e17bbc21d14d7fbe48c1c2eb729ee13b3fa3b
Abhay Singh Chauhan <75360601+arcaneAB@users.noreply.github.com>
2021-10-17 00:55:40 +0530

    staff side page creation

---
+++ customer/src/components/staff/OrderList.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { AiOutlineAlert, AiOutlineCheckSquare } from 'react-icons/ai'
+const brandHead = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  margin: '5%',
+}
+const orderHead = {
+  color: '#FA4A0C',
+  margin: '5%',
+}
+const listOut = {
+  listStyle: 'none',
+  margin: '0 3%',
+}
+const listIn = {
+  opacity: '0.6',
+  margin: '5% 4%',
+}
+const timeLine = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  margin: '0 2% 6%',
+  fontWeight: 'bold',
+}
+const listHead = {
+  color: '#FA4A0C',
+  display: 'flex',
+  justifyContent: 'space-between',
+  margin: '2%',
+  fontWeight: 'bold',
+}
+function OrderList() {
+  return (
+    <div>
+      <h1 style={brandHead}>
+        Swift Snack <AiOutlineAlert />
+      </h1>
+
+      <h2 style={orderHead}>Latest Orders</h2>
+      <ul style={listOut}>
+        <li>
+          <div style={listHead}>
+            Table: 7
+            <AiOutlineCheckSquare />
+          </div>
+          <ul style={listIn}>
+            <li>dish 1</li>
+            <li>dish 2</li>
+          </ul>
+          <div style={timeLine}>
+            <span>Time:</span>
+            <span>2:21 PM</span>
+          </div>
+        </li>
+        <li>
+          <div style={listHead}>
+            Table: 7
+            <AiOutlineCheckSquare />
+          </div>
+          <ul style={listIn}>
+            <li>dish 1</li>
+            <li>dish 2</li>
+          </ul>
+          <div style={timeLine}>
+            <span>Time:</span>
+            <span>2:21 PM</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
+}
+export default OrderList;
