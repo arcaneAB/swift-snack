ddfaa3678c26169c29b01c243a6f41f543a54efe
Abhay Singh Chauhan <75360601+arcaneAB@users.noreply.github.com>
2021-10-16 17:44:58 +0530

    done

--- customer/src/components/Navbar.jsx
+++ customer/src/components/Navbar.jsx
@@ -1,14 +1,25 @@
 import styled from 'styled-components'
-import { BsListCheck } from 'react-icons/bs'
+import { AiFillAlert } from 'react-icons/ai'
 import { CDBBadge, CDBContainer } from 'cdbreact'
 import { Link } from 'react-router-dom'
+import { IoFastFoodOutline } from 'react-icons/io5'
 import mobile from './Responsive'
+import Button from 'react-bootstrap/Button'
 
+const btn = {
+  backgroundColor: '#FA4A0C',
+  border: 'none',
+}
+const icon = {
+  color: '#FA4A0C',
+  border: 'none',
+  fontSize: '25px',
+}
 const Container = styled.div`
   width: 100%;
   height: 60px;
   margin: 15px;
-  ${mobile({ height: '140px', margin: '0' })};
+  ${mobile({ height: '150px', margin: '0' })};
 `
 
 const Wrapper = styled.div`
@@ -30,35 +41,56 @@ const MenuItem = styled.div`
   font-size: 1.5rem;
   cursor: pointer;
   margin: 0 15px;
-  ${mobile({ fontSize: '1rem', marginRight: '5px' })};
+  justify-content: space-between;
+  ${mobile({ fontSize: '1rem', margin: '5px', padding: '0 40px' })};
 `
 const Title = styled.h1`
+  text-decoration: none;
   font-size: 40px;
   width: 600px;
   padding-left: 100px;
-  ${mobile({ width: '60%', paddingLeft: '0' })};
+  ${mobile({ width: '80%', padding: '10px 0', textAlign: 'left' })};
+`
+const Register = styled.span`
+  color: grey;
+  position: absolute;
+  width: 58px;
+  height: 26px;
+  left: 311px;
+  top: 16px;
+  bottom: 40px ${mobile({ marginRight: '0px', padding: '0' })};
 `
 
 function Navbar() {
   return (
     <Container>
       <Wrapper>
-        <Title>Our Restaurant</Title>
+        <Title>
+          <Link to="/" style={{ color: '#000000' }}>
+            Swift Snack
+          </Link>
+        </Title>
+        <Link to="#">
+          <Register>Sign in</Register>
+        </Link>
         <Right>
-          <Link to="/">
-            <MenuItem>Home</MenuItem>
-          </Link>
-          <Link to="/">
-            <MenuItem>Register</MenuItem>
-          </Link>
           <Link to="#">
-            <MenuItem>Sign In</MenuItem>
+            <MenuItem>
+              <Button style={btn} border="pill">
+                Call Waiter <AiFillAlert />
+              </Button>
+            </MenuItem>
           </Link>
           <MenuItem>
             <Link to="/cart">
               <CDBContainer>
-                <BsListCheck />
-                <CDBBadge color="primary" size="small" borderType="pill">
+                <IoFastFoodOutline style={icon} />
+                <CDBBadge
+                  color="primary"
+                  style={btn}
+                  size="small"
+                  borderType="pill"
+                >
                   {/* {props.cart.length} */}5
                 </CDBBadge>
               </CDBContainer>
